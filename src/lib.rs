781af30a50cc93ac46e88ae81c8008812f4ae35a
Mickaël Salaün <mickael.salaun@ssi.gouv.fr>
2015-01-12 15:29:23 +0100

    iohandle: Add the FileDesc struct

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+// Copyright (C) 2015 Mickaël Salaün
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#![allow(staged_experimental)]
+
+extern crate libc;
+
+use std::os::unix::{AsRawFd, Fd};
+
+#[derive(Show)]
+#[cfg(unix)]
+pub struct FileDesc {
+    fd: Fd,
+    close_on_drop: bool,
+}
+
+impl FileDesc {
+    pub fn new(fd: Fd, close_on_drop: bool) -> FileDesc {
+        FileDesc {
+            fd: fd,
+            close_on_drop: close_on_drop,
+        }
+    }
+}
+
+impl Drop for FileDesc {
+    fn drop(&mut self) {
+        if self.close_on_drop {
+            unsafe { libc::close(self.fd); }
+        }
+    }
+}
+
+impl AsRawFd for FileDesc {
+    fn as_raw_fd(&self) -> Fd {
+        self.fd
+    }
+}
